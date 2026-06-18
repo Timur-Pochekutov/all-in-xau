36f1a5d77aecc0c7c61a41c237a95e42cfdec97a
Timur-Pochekutov <timurpochekutov@gmail.com>
2026-06-18 11:26:33 -0300

    api de precio de XAU en tiempo real con JS conectada y se recorto el precio total a dos decimales despues del punto

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hola desde JavaScript");
+async function getGoldPrice() {
+    const url = "https://api.gold-api.com/price/XAU";
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    const precio = document.querySelector(".precio");
+    precio.textContent = data.price.toFixed(2);
+}
+
+getGoldPrice();
