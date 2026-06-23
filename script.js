8c624a07335699131ceb3d41160fcce21baf04bf
Timur-Pochekutov <timurpochekutov@gmail.com>
2026-06-23 10:48:56 -0300

    frontend: maximo y minimo con colores, fix link mercados

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ async function actualizarDatos() {
     const data = await response.json();
     
     document.querySelector(".precio").textContent = data.precio.toFixed(2);
+
     const cambioEl = document.querySelector(".cambio");
     cambioEl.textContent = data.cambio_pct !== null ? data.cambio_pct.toFixed(2) + "%" : "-";
     cambioEl.className = "cambio";
+
     if (data.cambio_pct > 0) cambioEl.classList.add("positivo");
+
     else if (data.cambio_pct < 0) cambioEl.classList.add("negativo");
     else if (data.cambio_pct === 0) cambioEl.classList.add("neutro");
- }
-
+    
+    const maximoEl = document.querySelector(".maximo");
+    maximoEl.textContent = data.maximo !== null ? "↑ " + data.maximo.toFixed(2) : "↑ -";
+    
+    const minimoEl = document.querySelector(".minimo");
+    minimoEl.textContent = data.minimo !== null ? "↓ " + data.minimo.toFixed(2) : "↓ -";
+}
 actualizarDatos();
 setInterval(actualizarDatos, 10000);
