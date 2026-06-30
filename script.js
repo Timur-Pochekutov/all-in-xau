12d4d18340ffd6a317f6877c481ce5f9635365a5
Timur-Pochekutov <timurpochekutov@gmail.com>
2026-06-30 10:21:31 -0300

    actualizacion

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ async function actualizarMercados() {
         const response = await fetch ("https://all-in-xau-backend.onrender.com/mercados");
         const data = await response.json();
 
-        document.querySelector(".mkt-usd").textContent = data.USD !== null ? data.USD.toFixed(2) : "_";
-        document.querySelector(".mkt-eur").textContent = data.EUR !== null ? data.EUR.toFixed(2) : "_";
-        document.querySelector(".mkt-gbp").textContent = data.GBP !== null ? data.GBP.toFixed(2) : "_";
-        document.querySelector(".mkt-jpy").textContent = data.JPY !== null ? data.JPY.toFixed(2) : "_";
-        document.querySelector(".mkt-aud").textContent = data.AUD !== null ? data.AUD.toFixed(2) : "_";
+        document.querySelector(".mkt-usd-valor").textContent = data.USD !== null ? data.USD.toFixed(2) : "_";
+        document.querySelector(".mkt-eur-valor").textContent = data.EUR !== null ? data.EUR.toFixed(2) : "_";
+        document.querySelector(".mkt-gbp-valor").textContent = data.GBP !== null ? data.GBP.toFixed(2) : "_";
+        document.querySelector(".mkt-jpy-valor").textContent = data.JPY !== null ? data.JPY.toFixed(0) : "_";
+        document.querySelector(".mkt-aud-valor").textContent = data.AUD !== null ? data.AUD.toFixed(2) : "_";
     }
     actualizarMercados();
     setInterval(actualizarMercados, 10000);
@@ -39,11 +39,11 @@ async function actualizarETF() {
     const response = await fetch("https://all-in-xau-backend.onrender.com/etf")
     const data = await response.json();
 
-    document.querySelector(".etf-precio").textContent = data.precio !== null ? data.precio.toFixed(2) : "-";
+    document.querySelector(".mkt-valor").textContent = data.precio !== null ? data.precio.toFixed(2) : "-";
 
-    const cambioEl = document.querySelector(".etf-cambio");
+    const cambioEl = document.querySelector(".etf-cambio mkt-valor");
     cambioEl.textContent = data.cambio_pct !== null ? data.cambio_pct.toFixed(2) + "%" : "-";
-    cambioEl.className = "etf-cambio";
+    cambioEl.className = "etf-cambio mkt-valor";
     
     if (data.cambio_pct > 0) cambioEl.classList.add("positivo");
     else if (data.cambio_pct < 0) cambioEl.classList.add("negativo");
